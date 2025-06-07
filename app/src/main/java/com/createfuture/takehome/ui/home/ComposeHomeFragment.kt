ca0eda0a02a00a024d533080dc706958b2bcbc3d
Qandil11 <qandil.tariq11@gmail.com>
2025-06-07 20:13:35 +0100

    # Fixed 2: UI improvements

--- app/src/main/java/com/createfuture/takehome/ui/home/ComposeHomeFragment.kt
+++ app/src/main/java/com/createfuture/takehome/ui/home/ComposeHomeFragment.kt
@@ -32,6 +32,14 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Header
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+
 
 class ComposeHomeFragment : Fragment() {
     override fun onCreateView(
@@ -47,46 +55,98 @@ class ComposeHomeFragment : Fragment() {
             charactersBody.value = _characters.body()!!
         }
         setContent {
-            Column(modifier = Modifier.fillMaxSize().paint(painterResource(id = R.drawable.img_characters), contentScale = ContentScale.FillBounds).verticalScroll(rememberScrollState())) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+                    .paint(
+                        painterResource(id = R.drawable.img_characters),
+                        contentScale = ContentScale.FillBounds
+                    )
+            ){
                 if (charactersBody.value != null) {
                     for (character in charactersBody.value!!) {
-                        Row {
-                            Spacer(modifier = Modifier.size(16.dp))
-                            Column {
-                                Text(text = character.name,color = Color.White,fontSize = 16.sp)
-                                Row {
-                                    Text(text = "Culture: ",color = Color.White,fontSize = 16.sp)
-                                    Text(text = character.culture,color = Color.White,fontSize = 16.sp)
-                                }
-                                Row {
-                                    Text("Born: ",color = Color.White,fontSize = 16.sp)
-                                    Text(text = character.born,color = Color.White,fontSize = 16.sp)
-                                }
-                                Row {
-                                    Text(text = "Died: ",color = Color.White,fontSize = 16.sp)
-                                    Text(text = character.died,color = Color.White,fontSize = 16.sp)
-                                }
-                            }
-                            Spacer(modifier = Modifier.weight(1f))
-                            Column {
-                                Text("Seasons: ",color = Color.White,fontSize = 14.sp)
-                                var seasons = character.tvSeries.joinToString {
-                                    when (it) {
-                                        "Season 1" -> "I "
-                                        "Season 2" -> "II, "
-                                        "Season 3" -> "III, "
-                                        "Season 4" -> "IV, "
-                                        "Season 5" -> "V, "
-                                        "Season 6" -> "VI, "
-                                        "Season 7" -> "VII, "
-                                        "Season 8" -> "VIII"
-                                        else -> ""
+                        Column(   // Qandil 2: character will appear in a block, more readabile text,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 8.dp)
+                                .shadow(elevation = 4.dp, shape = RoundedCornerShape(12.dp))
+                                .background(Color(0xF01A1A1A), shape = RoundedCornerShape(12.dp))
+                                .padding(12.dp)
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(start = 16.dp), //Qandil 2: fix rows height issue
+                                verticalAlignment = Alignment.Top
+                            ) {
+                                Column(modifier = Modifier.weight(1f)) { //Qandil 2: fix rows height issue
+                                    Text(
+                                        text = character.name,
+                                        color = Color.White,
+                                        fontSize = 18.sp, // Qandil 2: prominent character name
+                                        fontWeight = FontWeight.Bold // Qandil 2: prominent character name
+
+                                    )
+                                    Row {
+                                        Text(
+                                            text = "Culture: ",
+                                            fontSize = 14.sp,
+                                            color = Color.White,
+                                            fontWeight = FontWeight.Medium,
+                                            modifier = Modifier.alignByBaseline()
+                                        )
+                                        Text(
+                                            text = character.culture,
+                                            color = Color.White,
+                                            fontSize = 14.sp,
+                                            modifier = Modifier.alignByBaseline()
+                                        )
+                                    }
+                                    Row {
+                                        Text("Born: ",  color = Color.White,
+                                            fontWeight = FontWeight.Medium, fontSize = 14.sp, modifier = Modifier.alignByBaseline())
+                                        Text(
+                                            text = character.born,
+                                            color = Color.White,
+                                            fontSize = 14.sp,
+                                            modifier = Modifier.alignByBaseline()
+                                        )
+                                    }
+                                    Row {
+                                        Text(text = "Died: ",  color = Color.White,
+                                            fontWeight = FontWeight.Medium, fontSize = 14.sp, modifier = Modifier.alignByBaseline())
+                                        Text(
+                                            text = character.died,
+                                            color = Color.White,
+                                            fontSize = 14.sp,
+                                            modifier = Modifier.alignByBaseline()
+                                        )
                                     }
                                 }
-                                Text(seasons,color = Color.White,fontSize = 14.sp)
+                                Column(modifier = Modifier.align(Alignment.CenterVertically)) { // Qandil 2: align seasons right
+                                    Text("Seasons: ", color = Color.White,
+                                        fontWeight = FontWeight.Medium, fontSize = 14.sp)
+                                    val seasons =
+                                        character.tvSeries.map {  // Qandil 2 : change it to map, it will remove extra commas and spaces.
+                                            when (it.trim()) {
+                                                "Season 1" -> "I"
+                                                "Season 2" -> "II"
+                                                "Season 3" -> "III"
+                                                "Season 4" -> "IV"
+                                                "Season 5" -> "V"
+                                                "Season 6" -> "VI"
+                                                "Season 7" -> "VII"
+                                                "Season 8" -> "VIII"
+                                                else -> ""
+                                            }
+                                        }.filter { it.isNotBlank() }.joinToString(", ")
+
+                                    Text(seasons, color = Color.White, fontSize = 14.sp)
+                                }
                             }
+                            Spacer(modifier = Modifier.size(16.dp))
                         }
-                        Spacer(modifier = Modifier.size(18.dp))
                     }
                 }
             }
